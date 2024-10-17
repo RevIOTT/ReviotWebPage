e2eab36dd7a0e10622b6d92aacb1d7ad70ac8548
RevIOTT <riotdynamcis.gmail.com>
2024-10-17 07:49:02 +0300

    Deployment errors

--- reviot-app/src/pages/LandingPage.tsx
+++ reviot-app/src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import NavBar from "../components/NavBar";
 import Home from "../components/Home";
 import Projects from "../components/Projects";
